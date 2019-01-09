2f8d78fc8be061cf6fe6c6ac0a544ef45e02da95
Calvin <calvinkinateder1@yahoo.com>
2019-01-09 09:45:05 -0500

    add calvins roobot

---
+++ zainsExperiments/calvinsrobot.java
@@ -0,0 +1,54 @@
+package zainsExperiments;
+
+import bc19.*;
+
+public class MyRobot extends BCAbstractRobot {
+	public int turn;
+
+	public Action turn() {
+    	turn++;
+
+    	if (me.unit == SPECS.CASTLE) { return castle(); }
+    	else if (me.unit == SPECS.CHURCH) { return church(); }
+    	else if (me.unit == SPECS.PILGRIM) { return pilgrim(); }
+    	else if (me.unit == SPECS.CRUSADER) { return crusader(); }
+    	else if (me.unit == SPECS.PROPHET) { return prophet(); }
+    	else if (me.unit == SPECS.PREACHER) { return preacher(); }
+    	return null;
+	}
+    
+    public Action castle()
+    {
+    	
+    }
+    
+    
+    public Action church()
+    {
+    	
+    }
+    
+    
+    public Action pilgrim()
+    {
+    	
+    }
+    
+    
+    public Action crusader()
+    {
+    	
+    }
+    
+    
+    public Action prophet()
+    {
+    	
+    }
+    
+    
+    public Action preacher()
+    {
+    	
+    }
+}
