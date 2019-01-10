f2eebd62fdf69e873dc84585adade2b9c2d58339
Zain <zainpi67@gmail.com>
2019-01-09 22:44:49 -0500

    Added autoAttack, but untested
    Added another helper method, this time to automatically attack best
    possible target. IT HAS NOT BEEN TESTED, since we have no movement code
    yet!

--- defaultStuff/MyRobot.java
+++ defaultStuff/MyRobot.java
@@ -1,11 +1,12 @@
 package bc19;
 
 import bc19.*;
-//import java.util.Arrays;
+import java.util.ArrayList;
 
 public class MyRobot extends BCAbstractRobot {
 	public int turn;
-	private boolean hRefl; // true iff reflected horizontally
+	private final int[] attackPriority = new int[] {4, 5, 3, 0, 1, 2}; // 0: Castle, 1: Church, 2: Pilgrim, 3: Crusader, 4: Prophet,
+	private boolean hRefl; // true iff reflected horizontally				 5: Preacher. Feel free to mess with order in your robots.
 	private int[][] fullMap; // 0: normal, 1: impassible, 2: karbonite, 3: fuel
 
 	public Action turn() {
@@ -83,7 +84,7 @@ public class MyRobot extends BCAbstractRobot {
 	}
 
 
-	public void getFMap()
+	public void getFMap() // makes fullMap
 	{
 		boolean[][] m = getPassableMap();
 		boolean[][] k = getKarboniteMap();
@@ -118,7 +119,7 @@ public class MyRobot extends BCAbstractRobot {
 		}
 	}
 
-	public boolean getReflDir()
+	public boolean getReflDir() // set hRefl
 	{
 		int top = (fullMap.length + 1) / 2;
 		int left = (fullMap[0].length + 1) / 2;
@@ -154,5 +155,55 @@ public class MyRobot extends BCAbstractRobot {
 		}
 
 		return true; // If it gets here, it's reflected both ways.
+	}
+	
+	public AttackAction autoAttack() // NOT TESTED: Attacks unit in attack range of type earliest in attackPriority, of lowest ID
+	{
+		Robot[] robs = getVisibleRobots();
+		ArrayList<Robot> priorRobs = new ArrayList<Robot>(); // only robots of highest priority type
+		int minRange, range;
+		if(me.id == 3 || me.id == 5)
+		{
+			minRange = 1;
+			range = 16;
+		}
+		else if(me.id == 4)
+		{
+			minRange = 16;
+			range = 64;
+		}
+		else
+		{
+			log("you're trying to attack with a non-combat robot and autoAttack() is gonna return an error");
+			minRange = 0;
+			range = 0;
+		}
+		
+		boolean found = false;
+		int i = 0;
+		while(!found) // make priorRobs
+		{
+			for(Robot rob : robs)
+			{
+				if(rob.team != me.team && rob.unit == attackPriority[i] && (rob.x - me.x) * (rob.x - me.x) + (rob.y - me.y) * 
+						(rob.y - me.y) <= range && (rob.x - me.x) * (rob.x - me.x) + (rob.y - me.y) * (rob.y - me.y) >= minRange)
+				{
+					found = true;
+					priorRobs.add(rob);
+				}
+			}
+			i++;
+		}
+		
+		int lowestID = 4097;
+		for(int j = 0; j < priorRobs.size(); j++)
+		{
+			if(priorRobs.get(j).id < lowestID)
+			{
+				lowestID = priorRobs.get(j).id;
+			}
+		}
+		
+		return attack(getRobot(lowestID).x - me.x, getRobot(lowestID).y - me.y);
 	}
 }
